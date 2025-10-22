33da6fa72b16d411efbaf52e357bac2662fbcab8
William Pettersson <contact@williampettersson.eu>
2025-10-22 15:53:18 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//////////////////////////
+//                      //
+//  dotcrawler          //
+//  Version 25.10a      //
+//                      //
+//  Written By:         //
+//  William Pettersson  //
+//                      //
+//////////////////////////
+
+using System;
+
+namespace dotcrawler
+{
+    class Program
+    {
+        // Main function
+        static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("dotcrawler\nv25.10a\nWilliam Pettersson\n");
+        }
+    }
+}
