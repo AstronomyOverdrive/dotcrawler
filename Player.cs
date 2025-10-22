af9ef5c4ea70e5272b6ee4b42f693a0b1958df3e
William Pettersson <contact@williampettersson.eu>
2025-10-22 16:51:23 +0200

    Add Player class

---
+++ Player.cs
@@ -0,0 +1,60 @@
+namespace dotcrawler
+{
+    public class Player
+    {
+        // Player information
+        private int x = 0;
+        private int y = 0;
+        private int hp = 5;
+        private string[] directions = ["West", "North", "East", "South"];
+        private int dirIndex = 0; // Changing an integer is both easier and require less memory than rotating array items
+
+        // Player position
+        public void SetPos(int newX, int newY)
+        {
+            // TODO: Add check for walls
+            x = newX;
+            y = newY;
+        }
+        public int[] GetPos()
+        {
+            return [x, y];
+        }
+
+        // Player health points
+        public void UpdateHp(int newHp)
+        {
+            hp = newHp;
+        }
+        public int GetHp()
+        {
+            return hp;
+        }
+
+        // Player rotation
+        public void Turn(bool right)
+        {
+            if (right)
+            {
+                dirIndex += 1;
+            }
+            else
+            {
+                dirIndex -= 1;
+            }
+            // Keep index within array range
+            if (dirIndex < 0)
+            {
+                dirIndex = 3;
+            }
+            if (dirIndex > 3)
+            {
+                dirIndex = 0;
+            }
+        }
+        public string GetDir()
+        {
+            return directions[dirIndex];
+        }
+    }
+}
