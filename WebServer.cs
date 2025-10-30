2ae902b3bc0b83f71c93cac4c9b33ea233a4fed4
William Pettersson <contact@williampettersson.eu>
2025-10-30 21:18:50 +0100

    Add WebServer class
    Route Get "/" serves index.html
    Route Post "/save/{data}" receives new map data
    Binds to 127.0.0.1:8080

---
+++ WebServer.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using System.Text.Json;
+
+namespace dotcrawler
+{
+    public class WebServer
+    {
+        // IP and port to run the server on
+        private string IPPort = "http://127.0.0.1:8080";
+        // For saving maps
+        List<MapInfo> maps = [];
+        string jsonFile = @"maps.json";
+
+        // Start webserver
+        public void Start()
+        {
+            // Create app
+            var builder = WebApplication.CreateBuilder([]);
+            var app = builder.Build();
+
+            // Serve html on route "/"
+            app.MapGet("/", (HttpContext context) =>
+            {
+                context.Response.Headers["Content-Type"] = "text/html";
+                return File.ReadAllText(@"index.html");
+            });
+
+            // Receive map data on route "/save/{data}"
+            app.MapPost("/save/{data}", (string? data) =>
+            {
+                // Check if client sent valid data
+                if (String.IsNullOrEmpty(data))
+                {
+                    return Results.StatusCode(400);
+                }
+                else
+                {
+                    // Load currently saved maps
+                    string readJSON = File.ReadAllText(jsonFile);
+                    maps = JsonSerializer.Deserialize<List<MapInfo>>(readJSON)!;
+                    // Get new map from client
+                    MapInfo newMap = JsonSerializer.Deserialize<MapInfo>(data)!;
+                    // Add new map to existing maps and save
+                    maps.Add(newMap);
+                    String writeJSON = JsonSerializer.Serialize(maps);
+                    File.WriteAllText(jsonFile, writeJSON);
+                    return Results.StatusCode(201);
+                }
+            });
+            // Start app
+            app.Run(IPPort);
+        }
+
+    }
+}
